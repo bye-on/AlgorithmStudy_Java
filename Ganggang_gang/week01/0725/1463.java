7cc50566acc68c03633c7730398d6e871af7c47a
jukang3570 <asekf100@naver.com>
2025-07-27 13:54:55 +0900

    0727

--- Ganggang_gang/week01/0725/1463.java
+++ Ganggang_gang/week01/0725/1463.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine().trim());
+        int n = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[N + 1];
+        int[] dp = new int[n + 1];
         dp[1] = 0;
 
-        for (int i = 2; i <= N; i++) {
+        for (int i = 2; i <= n; i++) {
             //1빼기
             int min = dp[i - 1] + 1;
 
@@ -27,7 +27,6 @@ public class Main {
             dp[i] = min;
         }
 
-        // 결과 출력
-        System.out.println(dp[N]);
+        System.out.println(dp[n]);
     }
 }
--- Ganggang_gang/week01/0725/2579.java
+++ Ganggang_gang/week01/0725/2579.java
@@ -7,24 +7,24 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine().trim());
-        int[] score = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            score[i] = Integer.parseInt(br.readLine().trim());
+        int n = Integer.parseInt(br.readLine());
+        int[] score = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            score[i] = Integer.parseInt(br.readLine());
         }
 
         //n이 1 혹은 2 때
-        if (N == 1) {
+        if (n == 1) {
             System.out.println(score[1]);
             return;
         }
-        if (N == 2) {
+        if (n == 2) {
             //두번째 계단으로 바로 시작하기
             System.out.println(score[1] + score[2]);
             return;
         }
 
-        int[][] dp = new int[N + 1][2];
+        int[][] dp = new int[n + 1][2];
 
         //초기값
         dp[1][0] = score[1];
@@ -34,12 +34,12 @@ public class Main {
         dp[2][1] = score[1] + score[2]; //두칸 해서 연속 밟을때
 
         //3번부터 n번까지 채우기
-        for (int i = 3; i <= N; i++) {
+        for (int i = 3; i <= n; i++) {
             dp[i][0] = Math.max(dp[i - 2][0], dp[i - 2][1]) + score[i];
             dp[i][1] = dp[i - 1][0] + score[i];
         }
 
-        int answer = Math.max(dp[N][0], dp[N][1]);
+        int answer = Math.max(dp[n][0], dp[n][1]);
         System.out.println(answer);
     }
 }
